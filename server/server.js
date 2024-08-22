b086c4209a0b3e25bb5596d87db69f1e9261c20c
Kartik Jevaji <9121718+nitrotap@users.noreply.github.com>
2024-08-22 00:37:06 -0600

    setting up ssl

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@
 
 const express = require('express');
 require('dotenv').config();
-
+const fs = require('fs');
+const https = require('https');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
@@ -11,9 +12,15 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 443; // Default HTTPS port
 const app = express();
 
+// SSL certificate options
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/citycodeassistant.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/citycodeassistant.com/cert.pem'),
+    ca: fs.readFileSync('/etc/letsencrypt/live/citycodeassistant.com/chain.pem')
+};
 
 const startServer = async () => {
     const server = new ApolloServer({
@@ -23,7 +30,7 @@ const startServer = async () => {
     });
     await server.start();
     server.applyMiddleware({ app });
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Use GraphQL at https://localhost:${PORT}${server.graphqlPath}`);
 };
 
 startServer();
@@ -43,7 +50,14 @@ app.get('*', (req, res) => {
 });
 
 db.once('open', () => {
-    app.listen(PORT, () => {
+    https.createServer(options, app).listen(PORT, '0.0.0.0', () => {
         console.log(`API server running on port ${PORT}!`);
     });
 });
+
+// Optionally, redirect HTTP traffic to HTTPS
+const http = require('http');
+http.createServer((req, res) => {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80, '0.0.0.0');
